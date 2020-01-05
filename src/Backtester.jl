1bde570cbd04aa046917dd3d6a883b32f66a7533
Yao Lu <luyaocns@gmail.com>
2020-01-05 11:48:04 +0800

    add upper bound

--- src/Backtester.jl
+++ src/Backtester.jl
@@ -2,10 +2,10 @@ __precompile__(true)
 
 module Backtester
 
-using Distributed, Statistics, Printf, Dates, DelimitedFiles, SHA
+using Distributed, Statistics, Printf, Dates, DelimitedFiles
 using Glob, DataStructures, Parameters, ProgressMeter, MacroTools
-using JSON, PyCall, BSON, StandardMarketData, Iconv
-using HDF5Utils, PandasLite, PyCallUtils, BSONMmap, MLSuiteBase
+using PyCall, BSON, StandardMarketData, MLSuiteBase
+using Iconv, HDF5Utils, PandasLite, PyCallUtils, BSONMmap
 using StatsBase: corspearman
 
 export Strategy, fit!, fit_code!, fit_roll!, fit_thresh!, backtest
