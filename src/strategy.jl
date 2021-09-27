5bf1e4536a707dcbde7302a23dcee9bbe130c8ef
Yao Lu <luyaocns@gmail.com>
2021-09-27 15:38:04 +0800

    update

--- src/strategy.jl
+++ src/strategy.jl
@@ -4,10 +4,10 @@
     持仓天数::Int = 1
     最多交易次数::Int = 1000
     禁止平今::Bool = false
-    夜盘最早开仓时间::Float32 = 0
+    夜盘最早开仓时间::Float32 = -Inf
     夜盘最晚开仓时间::Float32 = Inf
     夜盘最晚平仓时间::Float32 = Inf
-    早盘最早开仓时间::Float32 = 0
+    早盘最早开仓时间::Float32 = -Inf
     早盘最晚开仓时间::Float32 = Inf
     早盘最晚平仓时间::Float32 = Inf
 end
